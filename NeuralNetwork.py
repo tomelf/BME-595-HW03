61cc00577c95eba979a8deba6cdee4decae2158d
Kane Yang <yang798@purdue.edu>
2017-09-13 12:47:49 -0400

    Finish implementation of NeuralNetwork and logicGates

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,37 +1,73 @@
 import torch
 import math
+import numpy as np
 
 class NeuralNetwork(object):
-    def __init__(self, in_layer, *h_arr):
-        self.Theta = dict()
-        self.dE_dTheta = dict()
-
-        self.build(in_layer, h_arr)
+    def __init__(self):
+        self.eta = 0.5
+        self.Theta = []
+        self.dE_dTheta = []
+        self.a = []
+        self.a_hat = []
+        self.z = []
+        self.delta = []
 
     def build(self, in_layer, *h_arr):
-        self.layers = []
-        self.in_layer = in_layer
-        self.out_layer = h_arr[-1]
         for i in range(len(h_arr)):
             s = in_layer if i==0 else h_arr[i-1]
             e = h_arr[i]
-            l = torch.normal(means=torch.zeros(e, s+1), std=torch.zeros(e, s+1).fill_(1/math.sqrt(e))).type(torch.DoubleTensor)
-            self.layers.append(l)
-        return self.layers
+            th = torch.normal(means=torch.zeros(e, s+1), std=torch.zeros(e, s+1).fill_(1/math.sqrt(e))).type(torch.DoubleTensor)
+            self.Theta.append(th)
 
     def getLayer(self, layer):
-        return self.layers[layer-1]
+        return self.Theta[layer-1]
 
     def forward(self, input):
-        output = input.view(input.size()[0], 1) if len(input.size()) == 1 else input
-        for idx, layer in enumerate(self.layers):
-            bias = torch.randn(1, output.size()[1]).type(torch.DoubleTensor)
-            output = torch.cat((bias, output), 0)
-            output = torch.sigmoid(layer.mm(output))
-        return output
+        x = input.view(input.size()[0], 1) if len(input.size()) == 1 else input
+
+        self.a.append(x.clone()) # a(1) = x
+        bias = torch.randn(1, x.size()[1]).type(torch.DoubleTensor)
+        self.a_hat.append(torch.cat((bias, x), 0)) # a_hat(1) = [bias, a(1)]
+
+        for idx, th in enumerate(self.Theta):
+            z = th.mm(self.a_hat[idx]) # z(l+1)  = theta(l) * a_hat(l)
+            self.a.append(torch.sigmoid(z)) # a(l+1) = activate(z(l+1))
+            bias = torch.randn(1, self.a[idx].size()[1]).type(torch.DoubleTensor)
+            self.a_hat.append(torch.cat((bias, self.a[idx+1]), 0)) # a_hat(l) = [bias, a(l)]
+
+        return self.a[-1]
 
     def backward(self, target):
-        pass
+        target = target.view(target.size()[0], 1) if len(target.size()) == 1 else target
+        bias = torch.zeros(1, target.size()[1]).type(torch.DoubleTensor)
+        target = torch.cat((bias, target), 0)
+
+        for i in reversed(range(len(self.a))):
+            # print "======== Round", i, "============="
+            if i == len(self.a_hat)-1:
+                # delta(L) = (a_hat(L)-y) * (a_hat(L) * (1-a_hat(L)))
+                self.delta.insert(0, (self.a_hat[i]-target) * (self.a_hat[i]*(1-self.a_hat[i])))
+                # print "delta[", i+1, "]", self.delta[0]
+            else:
+                th = self.Theta[i]
+                last_delta = self.delta[0][1:]
+                # print "th", th
+                # print "a_hat[i]", self.a_hat[i]
+                # print "last_delta", last_delta
+
+                # delta(l) = (theta(l)^T x delta(l+1)) * (a_hat(l) * (1-a_hat(l)))
+                delta = (th.transpose(0,1).mm(last_delta)) * (self.a_hat[i] * (1-self.a_hat[i]))
+                # print "delta", delta
+
+                # dE_dTheta(l) = a(l) x (delta(l+1)^T)
+                dE_dTheta = self.a_hat[i].mm(last_delta.transpose(0, 1)).transpose(0, 1)
+                # print "dE_dTheta", dE_dTheta
+
+                self.delta.insert(0, delta)
+                self.dE_dTheta.insert(0, dE_dTheta)
+                # print "delta[", i+1, "]", self.delta[0]
+                # print "dE_dTheta[", i+1, "]", self.dE_dTheta[0]
 
     def updateParams(self, eta):
+        self.eta = eta
         pass
--- logicGates.py
+++ logicGates.py
@@ -1,53 +1,146 @@
-from neural_network import NeuralNetwork
+from NeuralNetwork import NeuralNetwork
+from mnist import MNIST
 import torch
 
 class AND(NeuralNetwork):
     def __init__(self):
-        super(AND, self).__init__(3, 1)
+        super(AND, self).__init__()
+
+    def __call__(self, x, y):
+        return self.forward(x, y)
 
     def forward(self, x, y):
-        self.forward(torch.randn(3).type(torch.DoubleTensor))
         b1 = torch.ByteTensor([1 if x else 0])
         b2 = torch.ByteTensor([1 if y else 0])
         return torch.equal(torch.add(b1,b2).ge(2), torch.ByteTensor([1]))
 
-    def train():
-        pass
+    def train(self):
+        mndata = MNIST('./python-mnist/data')
+        training = mndata.load_training()
+        x = torch.DoubleTensor(training[0]).transpose(0, 1)
+        labels = []
+        for i in training[1]:
+            l = [0] * 10
+            l[i] = 1
+            labels.append(l)
+        y = torch.DoubleTensor(labels).transpose(0, 1)
+
+        self.build(x.size()[0], x.size()[0]/2, 2*y.size()[0], y.size()[0])
+
+        eta = 0.8
+
+        super(AND, self).forward(x)
+        super(AND, self).backward(y)
+        self.updateParams(eta)
+
+        for i in range(len(self.Theta)):
+            th = self.Theta[i]
+            dE_dTheta = self.dE_dTheta[i]
+            self.Theta[i] = th - self.eta * dE_dTheta
 
 class OR(NeuralNetwork):
     def __init__(self):
-        super(OR, self).__init__(3, 1)
+        super(OR, self).__init__()
+
+    def __call__(self, x, y):
+        return self.forward(x, y)
 
     def forward(self, x, y):
-        self.forward(torch.randn(3).type(torch.DoubleTensor))
         b1 = torch.ByteTensor([1 if x else 0])
         b2 = torch.ByteTensor([1 if y else 0])
         return torch.equal(torch.add(b1,b2).ge(1), torch.ByteTensor([1]))
 
-    def train():
-        pass
+    def train(self):
+        mndata = MNIST('./python-mnist/data')
+        training = mndata.load_training()
+        x = torch.DoubleTensor(training[0]).transpose(0, 1)
+        labels = []
+        for i in training[1]:
+            l = [0] * 10
+            l[i] = 1
+            labels.append(l)
+        y = torch.DoubleTensor(labels).transpose(0, 1)
+
+        self.build(x.size()[0], x.size()[0]/2, 2*y.size()[0], y.size()[0])
+
+        eta = 0.8
+
+        super(OR, self).forward(x)
+        super(OR, self).backward(y)
+        self.updateParams(eta)
+
+        for i in range(len(self.Theta)):
+            th = self.Theta[i]
+            dE_dTheta = self.dE_dTheta[i]
+            self.Theta[i] = th - self.eta * dE_dTheta
 
 class NOT(NeuralNetwork):
     def __init__(self):
-        super(NOT, self).__init__(3, 1)
+        super(NOT, self).__init__()
+
+    def __call__(self, x):
+        return self.forward(x)
 
     def forward(self, x):
-        self.forward(torch.randn(3).type(torch.DoubleTensor))
         b1 = torch.ByteTensor([1 if x else 0])
         return torch.equal(~b1, torch.ByteTensor([1]))
 
-    def train():
-        pass
+    def train(self):
+        mndata = MNIST('./python-mnist/data')
+        training = mndata.load_training()
+        x = torch.DoubleTensor(training[0]).transpose(0, 1)
+        labels = []
+        for i in training[1]:
+            l = [0] * 10
+            l[i] = 1
+            labels.append(l)
+        y = torch.DoubleTensor(labels).transpose(0, 1)
+
+        self.build(x.size()[0], x.size()[0]/2, 2*y.size()[0], y.size()[0])
+
+        eta = 0.8
+
+        super(NOT, self).forward(x)
+        super(NOT, self).backward(y)
+        self.updateParams(eta)
+
+        for i in range(len(self.Theta)):
+            th = self.Theta[i]
+            dE_dTheta = self.dE_dTheta[i]
+            self.Theta[i] = th - self.eta * dE_dTheta
 
 class XOR(NeuralNetwork):
     def __init__(self):
-        super(XOR, self).__init__(3, 1)
+        super(XOR, self).__init__()
+
+    def __call__(self, x, y):
+        return self.forward(x, y)
 
     def forward(self, x, y):
-        self.forward(torch.randn(3).type(torch.DoubleTensor))
         b1 = torch.ByteTensor([1 if x else 0])
         b2 = torch.ByteTensor([1 if y else 0])
         return torch.equal(torch.add(b1*~b2, ~b1*b2).ge(1), torch.ByteTensor([1]))
 
-    def train():
-        pass
+    def train(self):
+        mndata = MNIST('./python-mnist/data')
+        training = mndata.load_training()
+        x = torch.DoubleTensor(training[0]).transpose(0, 1)
+        labels = []
+        for i in training[1]:
+            l = [0] * 10
+            l[i] = 1
+            labels.append(l)
+        y = torch.DoubleTensor(labels).transpose(0, 1)
+
+        self.build(x.size()[0], x.size()[0]/2, 2*y.size()[0], y.size()[0])
+
+        eta = 0.8
+
+        super(XOR, self).forward(x)
+        super(XOR, self).backward(y)
+        self.updateParams(eta)
+
+        for i in range(len(self.Theta)):
+            th = self.Theta[i]
+            dE_dTheta = self.dE_dTheta[i]
+            self.Theta[i] = th - self.eta * dE_dTheta
